df853612b227873a7a8f97149e65afa8025d3d6a
Jessi RC <jessy.ramirez23@gmail.com>
2021-05-15 16:45:58 -0400

    initial commit pending template updates and deployment

---
+++ client/build/precache-manifest.aa16045534ef03e8384fefb5faa58dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af95cb8d9f4fac2f22dfd12715d2742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ad106001a80342b0f9c",
+    "url": "/static/css/main.3fec954c.chunk.css"
+  },
+  {
+    "revision": "e5ae48d93e678d29e15e",
+    "url": "/static/js/2.29501955.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.29501955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad106001a80342b0f9c",
+    "url": "/static/js/main.c890919a.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
