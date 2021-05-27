2fc263bd3f98dae29d48a19474fac03df8646ae2
Jessi RC <jessy.ramirez23@gmail.com>
2021-05-26 22:07:03 -0400

    created second API call to get company name and show on FE

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,34 +1,55 @@
 import React, { useEffect, useState } from "react";
 import API from "../utils/API";
+import finnAPI from "../utils/finnhubAPI";
 import { useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
 
 function Results() {
-  const { symbol } = useParams();
-  const [results, setResults] = useState({});
+    const { symbol } = useParams();
+    const [results, setResults] = useState({});
+    const [companyName, setCompanyName] = useState();
 
-  useEffect(() => {
-    loadResults()
-  }, {});
+    useEffect(() => {
+        loadResults();
+        loadCompany();
+    }, {});
 
-  // Loads all companies and sets them to companies
-  function loadResults() {
-    API.getCompanyResults(symbol)
-      .then(res =>
-        setResults(res.data)
-      )
-      .catch(err => console.log(err));
-  };
+    // Loads all companies and sets them to companies
+    function loadResults() {
+        API.getCompanyResults(symbol)
+            .then(res =>
+                setResults(res.data)
+            )
+            .catch(err => console.log(err));
+            console.log('this is my company' + symbol)
+    }
 
-  return (
-    <Container fluid>
-      <Row>
-        <Col size="md-6">
-          {results.avgFemaleEntryLevel}
-        </Col>
-      </Row>
-    </Container>
-  );
+    function loadCompany() {
+        finnAPI.getCompany(symbol)
+            .then(res =>
+                setCompanyName(res.data.name)
+            )
+            .catch(err => console.log(err));
+    }
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col size="md-6">
+                  
+                    <h2>{results.avgFemaleEntryLevel}</h2>
+                    <h2>{results.avgFemaleMidLevel}</h2>
+                    <h2>{results.avgFemaleSeniorLevel}</h2>
+                    <h2>{results.avgMaleEntryLevel}</h2>
+                    <h2>{results.avgMaleMidLevel}</h2>
+                    <h2>{results.avgMaleSeniorLevel}</h2>
+                    
+                    {companyName}
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Results;
--- client/src/utils/finnhubAPI.js
+++ client/src/utils/finnhubAPI.js
@@ -10,7 +10,9 @@ export default {
         // https://finnhub.io/api/v1/stock/profile2?symbol=${query}&token=${key}
         // https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&&token=${key}
         // https://finnhub.io/api/v1/stock/profile2?symbol=AAPL&token=c2b9uhqad3i8k5kfmpp0
-
+    },
+    getCompany: function (symbol) {
+        return axios.get(`https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${key}`);
     }
 };
 
